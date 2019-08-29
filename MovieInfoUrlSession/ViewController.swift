18b1213c79910c95ce71c90701b538b5ced13491
IgorZ <izhizhirii@gmail.com>
2019-08-29 15:37:54 +0300

    Initial Commit

---
+++ MovieInfoUrlSession/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieInfoUrlSession
+//
+//  Created by Игорь on 8/29/19.
+//  Copyright © 2019 Igor Zhyzhyrii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
